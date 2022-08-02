eb81b19739edca3054b9e30db91c2a9aa8b9cd95
Arunrock <arungajjala11@gmail.com>
2022-08-02 22:50:15 +0530

    intial commit

---
+++ authservice/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into user (userid,upassword,uname) values ('admin','admin','admin');
+
+insert into user (userid,upassword,uname) values ('ramesh','ramesh','ramesh');
+insert into user (userid,upassword,uname) values ('suresh','suresh','suresh');
+insert into user (userid,upassword,uname) values ('praskash','praskash','praskash');
+insert into user (userid,upassword,uname) values ('rahul','rahul','rahul');
+insert into user (userid,upassword,uname) values ('raju','raju','raju');
